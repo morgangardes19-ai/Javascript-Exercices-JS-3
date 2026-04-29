10eda1e6c18429ef43f48682c13a08b36d3ea1c5
morgan1000 <morgan.gardes.19@gmail.com>
2026-04-29 16:25:02 +0200

    Exercices 3 et 4 à finir

--- Ex3/main.js
+++ Ex3/main.js
@@ -1,10 +1,22 @@
 let couleurVerte = document.querySelector("#vert");
 let couleurRouge = document.querySelector("#rouge");
 let couleurBleue = document.querySelector("#bleu");
+let colors = document.querySelector(".color")
 let paragraphe = document.querySelector("#text");
-console.log(paragraphe);
+// console.log(couleurVerte);
 
-couleurVerte.addEventListener("click", handleClickOnParagraphV);
-couleurRouge.addEventListener("click", handleClickOnParagraphR);
-couleurBleue.addEventListener("click", handleClickOnParagraphB);
+couleurVerte.addEventListener("click", handleClickOnColorV);
+couleurRouge.addEventListener("click", handleClickOnColorR);
+couleurBleue.addEventListener("click", handleClickOnColorB);
+
+
+// // function handleClickOnColorV() {
+//     // console.log(afficher);
+// //     paragraphe.classList.remove("color");
+// }
+
+function handleClickOnColorV() {
+    // console.log(afficher);
+    paragraphe.classList.add("vert");
+}
 
