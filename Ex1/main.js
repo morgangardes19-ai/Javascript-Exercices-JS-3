26503be2359d8cb459c8832e1bb2fd302091fbe2
morgan1000 <morgan.gardes.19@gmail.com>
2026-04-29 12:28:54 +0200

    Mise à jour exercice 1

---
+++ Ex1/main.js
@@ -0,0 +1,17 @@
+// ========== EXERCICE 1 =============
+let imageRecup = document.querySelector("#image1")
+// console.log(imageRecup);
+
+  image.addEventListener("mouseover", handleMouseoverOnImage);
+
+
+
+
+
+
+
+//   function handleMouseoverOnImage () {
+//     // console.log(imageRecup.src);
+//     imageRecup.src = "./image1.jpg"
+
+// }
